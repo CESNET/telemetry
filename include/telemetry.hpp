0e0434a26221a8a43ce14f763108a01f2b3b2fb5
Pavel Siska <siska@cesnet.cz>
2024-03-12 14:43:17 +0100

    telemetry - introduce telemetry.hpp - the main devel header

---
+++ include/telemetry.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @brief The main devel header for telemetry
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include <telemetry/aggFile.hpp>
+#include <telemetry/aggMethod.hpp>
+#include <telemetry/content.hpp>
+#include <telemetry/directory.hpp>
+#include <telemetry/file.hpp>
+#include <telemetry/holder.hpp>
+#include <telemetry/node.hpp>
+#include <telemetry/utility.hpp>
