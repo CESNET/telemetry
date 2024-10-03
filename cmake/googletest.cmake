3aa0180a4e294c0f3ad538afd9e8afd7cdd5418b
Pavel Siska <siska@cesnet.cz>
2024-10-03 22:31:58 +0200

    cmake - googletest - remove -Wsign-conversion cxx flag

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -12,4 +12,10 @@ FetchContent_Declare(
 	GIT_REPOSITORY https://github.com/google/googletest.git
 	GIT_TAG        v1.14.0
 )
+
+# Library does not compile with -Wsign-conversion that we use in builds
+string(REPLACE "-Wsign-conversion " " " CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ")
+string(REPLACE "-Wsign-conversion " " " CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ")
+string(REPLACE "-Wsign-conversion " " " CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ")
+
 FetchContent_MakeAvailable(googletest)
